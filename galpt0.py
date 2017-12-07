c1944e84b6e4acccaf2dea5282f557ec46695b18
galpt <34034590+galpt@users.noreply.github.com>
2017-12-07 20:16:35 +0700

    Update galpt0.py

--- galpt0.py
+++ galpt0.py
@@ -226,11 +226,11 @@ def RECEIVE_MESSAGE(op):
 				if (".enc " in msg.text):
 		   			enc = msg.text.replace(".enc ","")
 					enc0 = enc.encode('base64','strict')
-					enc1 = binascii.hexlify(enc0)
+					enc1 = codecs.encode(enc0, 'latin1')
 		   			sendMessage(msg.to, "" + enc1)
 				if (".dec " in msg.text):
 		   			dec = msg.text.replace(".dec ","")
-					dec0 = binascii.unhexlify(dec)
+					dec0 = codecs.decode(dec, 'latin1')
 					dec1 = dec0.decode('base64','strict')
 		   			sendMessage(msg.to, "" + dec1)
 			else:
@@ -452,11 +452,11 @@ def SEND_MESSAGE(op):
 				if (".enc " in msg.text):
 		   			enc = msg.text.replace(".enc ","")
 					enc0 = enc.encode('base64','strict')
-					enc1 = binascii.hexlify(enc0)
+					enc1 = codecs.encode(enc0, 'latin1')
 		   			sendMessage(msg.to, "" + enc1)
 				if (".dec " in msg.text):
 		   			dec = msg.text.replace(".dec ","")
-					dec0 = binascii.unhexlify(dec)
+					dec0 = codecs.decode(dec, 'latin1')
 					dec1 = dec0.decode('base64','strict')
 		   			sendMessage(msg.to, "" + dec1)
 				if msg.text == ".ping":
@@ -647,11 +647,11 @@ def SEND_MESSAGE(op):
 				if (".enc " in msg.text):
 		   			enc = msg.text.replace(".enc ","")
 					enc0 = enc.encode('base64','strict')
-					enc1 = binascii.hexlify(enc0)
+					enc1 = codecs.encode(enc0, 'latin1')
 		   			sendMessage(msg.to, "" + enc1)
 				if (".dec " in msg.text):
 		   			dec = msg.text.replace(".dec ","")
-					dec0 = binascii.unhexlify(dec)
+					dec0 = codecs.decode(dec, 'latin1')
 					dec1 = dec0.decode('base64','strict')
 		   			sendMessage(msg.to, "" + dec1)
 				if msg.text == ".about":
