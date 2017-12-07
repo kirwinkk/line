6b95168e6cc1cb65fec6b14636bd79111af45229
galpt <34034590+galpt@users.noreply.github.com>
2017-12-07 21:01:11 +0700

    Update galpt0.py

--- galpt0.py
+++ galpt0.py
@@ -226,11 +226,11 @@ def RECEIVE_MESSAGE(op):
 				if (".enc " in msg.text):
 		   			enc = msg.text.replace(".enc ","")
 					enc0 = enc.encode('base64','strict')
-					enc1 = base32.encode(enc0)
+					enc1 = base64.b32encode(enc0)
 		   			sendMessage(msg.to, "" + enc1)
 				if (".dec " in msg.text):
 		   			dec = msg.text.replace(".dec ","")
-					dec0 = base32.decode(dec)
+					dec0 = base64.b32decode(dec)
 					dec1 = dec0.decode('base64','strict')
 		   			sendMessage(msg.to, "" + dec1)
 			else:
@@ -452,11 +452,11 @@ def SEND_MESSAGE(op):
 				if (".enc " in msg.text):
 		   			enc = msg.text.replace(".enc ","")
 					enc0 = enc.encode('base64','strict')
-					enc1 = base32.encode(enc0)
+					enc1 = base64.b32encode(enc0)
 		   			sendMessage(msg.to, "" + enc1)
 				if (".dec " in msg.text):
 		   			dec = msg.text.replace(".dec ","")
-					dec0 = base32.decode(dec)
+					dec0 = base64.b32decode(dec)
 					dec1 = dec0.decode('base64','strict')
 		   			sendMessage(msg.to, "" + dec1)
 				if msg.text == ".ping":
@@ -647,11 +647,11 @@ def SEND_MESSAGE(op):
 				if (".enc " in msg.text):
 		   			enc = msg.text.replace(".enc ","")
 					enc0 = enc.encode('base64','strict')
-					enc1 = base32.encode(enc0)
+					enc1 = base64.b32encode(enc0)
 		   			sendMessage(msg.to, "" + enc1)
 				if (".dec " in msg.text):
 		   			dec = msg.text.replace(".dec ","")
-					dec0 = base32.decode(dec)
+					dec0 = base64.b32decode(dec)
 					dec1 = dec0.decode('base64','strict')
 		   			sendMessage(msg.to, "" + dec1)
 				if msg.text == ".about":
