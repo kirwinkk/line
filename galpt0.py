d5403f8546358e0df2b4d8662a5c395f053a400c
galpt <34034590+galpt@users.noreply.github.com>
2017-12-07 20:26:25 +0700

    Update galpt0.py

--- galpt0.py
+++ galpt0.py
@@ -4,7 +4,7 @@ from LineAlpha.LineApi import LineTracer
 from LineAlpha.LineThrift.ttypes import Message
 from LineAlpha.LineThrift.TalkService import Client
 import time, datetime, random ,sys, re, string, os, json, codecs, threading, glob, subprocess, webbrowser, ConfigParser
-import base64, mechanize, tweepy, binascii
+import base64, mechanize, tweepy, binascii, utf-8
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
